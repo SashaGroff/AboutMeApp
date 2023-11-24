d61e991b8cc65349718f5674fc7877dc9b4bf694
SashaGroff <148804078+SashaGroff@users.noreply.github.com>
2023-11-24 23:01:06 +0300

    Complite

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -9,27 +9,66 @@ import UIKit
 
 final class LoginViewController: UIViewController {
     
-   private let userName = "Student"
-   private let userPassword = "pass1234"
+    private let userName = "Student"
+    private let userPassword = "pass1234"
     
     @IBOutlet var userNameTF: UITextField!
-    @IBOutlet var passwordTF: UITextField!
+    @IBOutlet var userPasswordTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
     
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?
+    ) -> Bool {
+        guard userNameTF.text == userName, userPasswordTF.text == userPassword else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password"
+            )
+            return false
+        }
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let greetingScreen = segue.destination as? WelcomeViewController
+        
+        greetingScreen?.greetingName = userName
+    }
+   
+//MARK: IBActions
+    @IBAction func remaindUserNameAction() {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is \(userName) 😉")
+    }
+    
+    @IBAction func remaindPasswordAction() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is \(userPassword) 😉")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        userPasswordTF.text = ""
+    }
+    
+}
 
-    @IBAction func logInAction() {
-    }
-    
 
-    @IBAction func remaindAuthorizationDataActiont(_ sender: UIButton) {
-    }
-    
+// MARK: UIAlertController
+extension LoginViewController{
     private func showAlert(withTitle title: String, andMessage message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.userPasswordTF?.text = ""
+        }
+        alert.addAction(okAction)
         present(alert, animated: true)
     }
 }
-
--- AboutMeApp/WelcomeViewController.swift
+++ AboutMeApp/WelcomeViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 final class WelcomeViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
     
+    var greetingName: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addBackgroundWithGradient()
+        greetingLabel.text = "Welcome, \(greetingName ?? "")"
     }
-
+    
+    private func addBackgroundWithGradient() {
+        // Идею честно взял здесь: https://www.youtube.com/watch?v=Qk_H0mlSIQc
+        let gradientBackground = CAGradientLayer()
+        gradientBackground.colors = [UIColor.systemPink.cgColor, UIColor.systemBlue.cgColor]
+        gradientBackground.frame = view.bounds
+        
+        gradientBackground.startPoint = CGPoint(x: 0, y: 0)
+        gradientBackground.endPoint = CGPoint(x: 0, y: 1)
+        
+        view.layer.insertSublayer(gradientBackground, at: 0)
+    }
 }
