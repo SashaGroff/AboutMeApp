b13aa4fbbce506e6b1293e550f00f018b8650e4b
SashaGroff <148804078+SashaGroff@users.noreply.github.com>
2023-12-02 16:47:44 +0300

    Fix

--- AboutMeApp/ViewControllers/AboutMeViewController.swift
+++ AboutMeApp/ViewControllers/AboutMeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AboutMeViewController: UIViewController {
+final class AboutMeViewController: UIViewController {
     
      // MARK: - IB Outlets
     @IBOutlet var bioLabel: UILabel!
